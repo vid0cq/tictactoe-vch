dee6932c691262334cf18909da75d060c3b23264
vid0cq <victor.chira@gmail.com>
2018-05-13 15:34:14 +0300

    no language ext version

---
+++ tictactoe-vch/Logic.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace tictactoe_vch
+{
+    class Logic
+    {
+
+        #region Move
+
+        public (int, int) Move(BoxState[,] table)
+        {
+            var finalResult = Int32.MinValue;
+            var (row, col) = (0, 0);
+            foreach (var (urow,ucol) in GetUnusedBoxes(table))
+            {
+                var crtResult = ComputeMove((BoxState[,])table.Clone(), urow, ucol, false);
+                if (crtResult > finalResult)
+                {
+                    finalResult = crtResult;
+                    (row, col) = (urow, ucol);
+                }
+            }
+
+            return (row, col);
+        }
+
+        public static int ComputeMove(BoxState[,] table, int row, int col, bool playerTurn)
+        {
+            table[row, col] = playerTurn ? BoxState.X : BoxState.O;
+            if (won(table, row, col)) return playerTurn ? -1 : 1;
+            if (full(table)) return 0;
+
+            var finalResult = playerTurn ? Int32.MinValue : Int32.MaxValue;
+            foreach (var (urow, ucol) in GetUnusedBoxes(table))
+            {
+                var result = ComputeMove((BoxState[,])table.Clone(), urow, ucol, playerTurn ^ true);
+                if(playerTurn && result>finalResult) finalResult = result;
+                else if(!playerTurn && result < finalResult) finalResult = result;
+            }
+
+            return finalResult;
+        }
+
+        private static IEnumerable<(int, int)> GetUnusedBoxes(BoxState[,] table)
+        {
+            var unusedBoxes = new List<(int, int)>();
+            for (int i = 0; i < table.GetLength(0); i++)
+                for (int j = 0; j < table.GetLength(0); j++)
+                    if (table[i, j] == BoxState.Unused) unusedBoxes.Add((i, j));
+
+            return unusedBoxes;
+        }
+
+        #endregion Move
+
+        #region End of game
+
+        public static bool full(BoxState[,] table)
+        {
+            for (int i = 0; i < table.GetLength(0); i++)
+                for (int j = 0; j < table.GetLength(0); j++)
+                    if (table[i, j] == BoxState.Unused) return false;
+
+            return true;
+        }
+
+        public static bool won(BoxState[,] table, int row, int col)
+        {
+            return checkRow(table, row, col) || checkCol(table, row, col)
+                || checkDiag(table, row, col) || checkSecDiag(table, row, col);
+        }
+
+        private static bool checkRow(BoxState[,] table, int row, int col)
+        {
+            BoxState state = table[row, col];
+            for (int i = 0; i < table.GetLength(0); i++)
+                if (table[i, col] != state) return false;
+
+            return true;
+        }
+
+        private static bool checkCol(BoxState[,] table, int row, int col)
+        {
+            BoxState state = table[row, col];
+            for (int i = 0; i < table.GetLength(0); i++)
+                if (table[row, i] != state) return false;
+
+            return true;
+        }
+
+        private static bool checkDiag(BoxState[,] table, int row, int col)
+        {
+            if (row != col) return false;
+
+            BoxState state = table[row, col];
+            for (int i = 0; i < table.GetLength(0); i++)
+                if (table[i, i] != state) return false;
+
+            return true;
+        }
+
+        private static bool checkSecDiag(BoxState[,] table, int row, int col)
+        {
+            if (row + col != table.GetLength(0) - 1) return false;
+
+            BoxState state = table[row, col];
+            for (int i = 0; i < table.GetLength(0); i++)
+                if (table[i, table.GetLength(0) - (i + 1)] != state) return false;
+
+            return true;
+        }
+
+        #endregion End of game
+    }
+}
---
+++ tictactoe-vch/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static tictactoe_vch.Logic;
+
+namespace tictactoe_vch
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private BoxState[,] table;
+        private Logic logic;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Start();
+        }
+
+        private void Start()
+        {
+            table = new BoxState[3, 3];
+            logic = new Logic();
+
+            tictactoeGrid.Children.Cast<Button>().ToList().ForEach(btn => btn.Content = String.Empty);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var button = ((Button)sender);
+            var col = Grid.GetColumn(button);
+            var row = Grid.GetRow(button);
+
+            if (table[row, col] != BoxState.Unused) return;
+
+            table[row, col] = BoxState.X;
+            button.Content = "X";
+
+            if (won(table, row, col)) ShowMessage("Human Player Won!");
+            else if (full(table)) ShowMessage("It's a tie");
+            else
+            {
+                var (mrow,mcol) = logic.Move(table);
+                table[mrow, mcol] = BoxState.O;
+                ((Button)tictactoeGrid.FindName("Button" + mrow + mcol)).Content="O";
+                if (won(table, mrow, mcol)) ShowMessage("Computer Won!");
+                else if (full(table)) ShowMessage("It's a tie");
+            }
+        }
+
+        private void ShowMessage(String message)
+        {
+            MessageBox.Show(message);
+            Start();
+        }
+
+       
+    }
+}
